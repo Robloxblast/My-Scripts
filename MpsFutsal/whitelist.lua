c5893e7accf2c273cf9a2cb7c312a812e2c1bee6
wrestonbest <wreston7@hotmail.com>
2024-07-06 01:15:38 +0300

    Create whitelist.lua

---
+++ MpsFutsal/whitelist.lua
@@ -0,0 +1,30 @@
+local whitelist = {
+    "wrestonmain", --owner
+    "Ben_Muhammed17", -- admin
+    
+    
+}
+
+local function trim(s)
+    return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
+local function isPlayerWhitelisted(playerName)
+    local trimmedPlayerName = trim(playerName:lower())
+    
+    for _, name in ipairs(whitelist) do
+        local trimmedWhitelistName = trim(name:lower())
+        trimmedWhitelistName) 
+        if trimmedWhitelistName == trimmedPlayerName then
+            l
+            return true
+        end
+    end
+    print("Player is not whitelisted") 
+    return false
+end
+
+return {
+    isPlayerWhitelisted = isPlayerWhitelisted
+}
+
